81e816f98553bc6e450886894fc630a9ae718f48
Adolfo Abegg <adolfoabegg@gmail.com>
2021-06-21 18:42:25 -0400

    NNEO-9686 polished the examples

---
+++ examples/oneclick_link.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__ . '/../vendor/autoload.php';
+
+$sharedSecret = 'Your_Vendo_Shared_Secret__get_it_from_us';
+$oneclikLink = new VendoSdk\Url\Oneclick($sharedSecret);
+$oneclikLink->setSubscription(222333444555);
+$oneclikLink->setOffer(222);
+$oneclikLink->setSuccessUrl('http://google.com');
+$oneclikLink->setDeclineUrl('http://yahoo.com?q=test');
+$oneclikLink->setRef('refxyz_999_www');
+
+$url = $oneclikLink->getSignedUrl();
+
+//redirect the user to Vendo's payment page
+//header('Location: ' . $url);
+echo $url . PHP_EOL;
--- examples/standard_join_link.php
+++ examples/standard_join_link.php
@@ -1,16 +1,18 @@
 <?php
-include __DIR__ . '../vendor/autoload.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 $sharedSecret = 'Your_Vendo_Shared_Secret__get_it_from_us';
-$joinLink = new \VendoSdk\Url\Join($sharedSecret);
+$joinLink = new VendoSdk\Url\Join($sharedSecret);
 $joinLink->setSite(2);
 $joinLink->setOffers([123,222,333]);
 $joinLink->setSelectedOffer(222);
+$joinLink->setAffiliateId(0);//zero means organic traffic or search engine traffic
 $joinLink->setSuccessUrl('http://google.com');
 $joinLink->setDeclineUrl('http://yahoo.com?q=test');
 $joinLink->setRef('refxyz_999_www');
 
-$url = $joinLink->getSignedUrl();
+$url = $joinLink->getUrl();
 
 //redirect the user to Vendo's payment page
-header('Location: ' . $url);
+//header('Location: ' . $url);
+echo $url . PHP_EOL;
