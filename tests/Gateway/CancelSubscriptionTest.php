2a5fbaa0a5a4f7b28cbad60b23973e02f83c83ea
Zbigniew Więch <zbigniew.wiech@vendoservices.com>
2022-06-15 14:07:58 +0200

    unit test: cancel subscription

--- tests/Gateway/CancelSubscriptionTest.php
+++ tests/Gateway/CancelSubscriptionTest.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use VendoSdk\Gateway\CancelSubscription;
 use VendoSdk\Gateway\CapturePayment;
 use VendoSdk\Vendo;
 
@@ -13,12 +14,12 @@ class CancelSubscriptionTest extends \PHPUnit\Framework\TestCase
 {
     public function testCancelSubscriptionSuccess()
     {
-        $payment = new CapturePayment();
-        $payment->setIsTest(true);
-        $payment->setApiSecret('test-secret');
-        $payment->setIsTest(true);
-        $payment->setMerchantId(1234567);
-        $payment->setTransactionId(87654321);
+        $cancelSubscription = new CancelSubscription();
+        $cancelSubscription->setIsTest(true);
+        $cancelSubscription->setApiSecret('test-secret');
+        $cancelSubscription->setIsTest(true);
+        $cancelSubscription->setMerchantId(1234567);
+        $cancelSubscription->setSubscriptionId(87654321);
 
         $httpClient = $this->createMock(Client::class);
 
@@ -26,73 +27,27 @@ class CancelSubscriptionTest extends \PHPUnit\Framework\TestCase
         $response->method('getBody')->willReturn(json_encode([
             'status' => Vendo::GATEWAY_STATUS_OK,
             'request_id' => 234,
-            'transaction' => [
+            'subscription' => [
                 'id' => 9876543,
-                'amount' => 10.23,
-                'currency' => 'USD',
-                'datetime' => '2021-11-10 14:52:34',
             ],
         ]));
         $httpClient->method('send')->willReturn($response);
 
-        $payment->setHttpClient($httpClient);
-        $payment->postRequest();
+        $cancelSubscription->setHttpClient($httpClient);
+        $cancelSubscription->postRequest();
 
-        $this->assertEquals(true, $payment->isTest());
-        $this->assertEquals('https://secure.vend-o.com/api/gateway/capture', $payment->getApiEndpoint());
-        $this->assertEquals('test-secret', $payment->getApiSecret());
-        $this->assertEquals(1234567, $payment->getMerchantId());
-        $this->assertEquals(87654321, $payment->getTransactionId());
-        $this->assertEquals('{"status":1,"request_id":234,"transaction":{"id":9876543,"amount":10.23,"currency":"USD","datetime":"2021-11-10 14:52:34"}}', $payment->getRawResponse());
+        $this->assertEquals(true, $cancelSubscription->isTest());
+        $this->assertEquals(Vendo::BASE_URL . '/api/gateway/cancel-subscription', $cancelSubscription->getApiEndpoint());
+        $this->assertEquals('test-secret', $cancelSubscription->getApiSecret());
+        $this->assertEquals(1234567, $cancelSubscription->getMerchantId());
+        $this->assertEquals(87654321, $cancelSubscription->getSubscriptionId());
+        $this->assertEquals('{"status":1,"request_id":234,"subscription":{"id":9876543}}', $cancelSubscription->getRawResponse());
 
         $this->assertEquals('{
     "api_secret": "test-secret",
-    "merchant_id": 1234567,
     "is_test": 1,
-    "transaction_id": 87654321
-}', $payment->getRawRequest(true));
-    }
-
-    public function testCapturePaymentClientException()
-    {
-        $payment = new CapturePayment();
-        $httpClient = $this->createMock(Client::class);
-        $payment->setHttpClient($httpClient);
-
-        $payment->setApiSecret('test-secret');
-        $payment->setMerchantId(1234567);
-        $payment->setTransactionId(87654321);
-
-        $response = $this->createMock(ResponseInterface::class);
-        $response->method('getBody')->willReturn(json_encode([
-            'status' => Vendo::GATEWAY_STATUS_NOT_OK,
-            'error_code' => 999,
-            'error_message' => 'Test client exception',
-        ]));
-
-        $request = $this->createMock(RequestInterface::class);
-        $httpClient->method('send')->willThrowException(new ClientException('Test ServerException', $request, $response));
-
-        $payment->postRequest();
-        $this->assertEquals('{"status":0,"error_code":999,"error_message":"Test client exception"}', $payment->getRawResponse());
-    }
-
-    public function testCapturePaymentServerException()
-    {
-        $payment = new CapturePayment();
-        $httpClient = $this->createMock(Client::class);
-        $payment->setHttpClient($httpClient);
-
-        $payment->setApiSecret('test-secret');
-        $payment->setMerchantId(1234567);
-        $payment->setTransactionId(87654321);
-
-        $response = $this->createMock(ResponseInterface::class);
-        $request = $this->createMock(RequestInterface::class);
-        $httpClient->method('send')->willThrowException(new ServerException('Test ServerException', $request, $response));
-
-        $this->expectException(\Exception::class);
-        $this->expectErrorMessage('A server exception occurred. If this persists then contact Vendo Client Support');
-        $payment->postRequest();
+    "merchant_id": 1234567,
+    "subscription_id": 87654321
+}', $cancelSubscription->getRawRequest(true));
     }
 }
