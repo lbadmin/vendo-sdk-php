d02fb40e57617a9a7cb7b76af67487c666225bec
Adolfo Abegg <adolfoabegg@gmail.com>
2023-08-03 12:02:09 -0400

    fixed unit test

--- tests/Url/JoinTest.php
+++ tests/Url/JoinTest.php
@@ -22,7 +22,7 @@ class JoinTest extends \PHPUnit\Framework\TestCase
 
         $linkTo = $url->getUrl();
 
-        $expectedUrl =  'https://secure.vend-o.com/v/signup?site=1&affiliate_id=0&decline_url=http%3A%2F%2Fyahoo.com&success_url=http%3A%2F%2Fgoogle.com&ref=xyz&username=foo&password_encrypted=%D5%22%BB%B1%87%7D%C9%09%F0%DC%9C%8A%C9%97w%D6&email=foo%40bazdummy.com&sdkv=' . Vendo::SDK_VERSION;
+        $expectedUrl =  'https://secure.vend-o.com/v/signup?site=1&affiliate_id=0&decline_url=http%3A%2F%2Fyahoo.com&success_url=http%3A%2F%2Fgoogle.com&ref=xyz&username=foo&password_encrypted=1&password=%D5%22%BB%B1%87%7D%C9%09%F0%DC%9C%8A%C9%97w%D6&email=foo%40bazdummy.com&sdkv=' . Vendo::SDK_VERSION;
 
         $this->assertEquals($expectedUrl, $linkTo);
 
