84b737382ed8f66782dbdb9fb9d4e6cb5734a434
Zbigniew Więch <zbigniew.wiech@nythross.com>
2023-08-08 11:08:23 +0200

    unit test update sub payment method success

--- tests/S2S/ChangeSubscriptionTest.php
+++ tests/S2S/ChangeSubscriptionTest.php
@@ -114,7 +114,64 @@ class ChangeSubscriptionTest extends \PHPUnit\Framework\TestCase
 }', $changeSubscription->getRawRequest(true));
     }
 
-    public function testChangeSubscriptionErrorBadParam()
+    public function testChangeSubscriptionPaymentMethodSuccessVerificationRequired()
+    {
+        $changeSubscription = new ChangeSubscription();
+        $changeSubscription->setIsTest(true);
+        $changeSubscription->setApiSecret('test-secret');
+        $changeSubscription->setIsTest(true);
+        $changeSubscription->setMerchantId(1234567);
+        $changeSubscription->setSubscriptionId(87654321);
+
+        $ccDetails = new \VendoSdk\S2S\Request\Details\PaymentMethod\CreditCard();
+        $ccDetails->setNameOnCard('John Doe');
+        $ccDetails->setCardNumber('4000012892688323');
+        $ccDetails->setExpirationMonth('05');
+        $ccDetails->setExpirationYear('2029');
+        $ccDetails->setCvv(123);
+        $changeSubscription->setPaymentDetails($ccDetails);
+
+        $httpClient = $this->createMock(Client::class);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getBody')->willReturn(json_encode([
+            'status' => Vendo::S2S_STATUS_VERIFICATION_REQUIRED,
+            'request_id' => 234,
+            'verification_id' => 12345,
+            'verification_url' => 'http://host/verification_url',
+            'subscription' => [
+                'id' => 9876543,
+            ],
+        ]));
+        $httpClient->method('send')->willReturn($response);
+
+        $changeSubscription->setHttpClient($httpClient);
+        $changeSubscription->postRequest();
+
+        $this->assertEquals(true, $changeSubscription->isTest());
+        $this->assertEquals(Vendo::BASE_URL . '/api/gateway/change-subscription', $changeSubscription->getApiEndpoint());
+        $this->assertEquals('test-secret', $changeSubscription->getApiSecret());
+        $this->assertEquals(1234567, $changeSubscription->getMerchantId());
+        $this->assertEquals(87654321, $changeSubscription->getSubscriptionId());
+        $this->assertEquals('{"status":2,"request_id":234,"verification_id":12345,"verification_url":"http:\/\/host\/verification_url","subscription":{"id":9876543}}', $changeSubscription->getRawResponse());
+
+        $this->assertEquals('{
+    "api_secret": "test-secret",
+    "is_test": 1,
+    "merchant_id": 1234567,
+    "subscription_id": 87654321,
+    "payment_details": {
+        "payment_method": "card",
+        "card_number": "4000012892688323",
+        "expiration_month": "05",
+        "expiration_year": "2029",
+        "cvv": "123",
+        "name_on_card": "John Doe"
+    }
+}', $changeSubscription->getRawRequest(true));
+    }
+
+    public function testChangeSubscriptionScheduleErrorBadParam()
     {
         $changeSubscription = new ChangeSubscription();
         $changeSubscription->setIsTest(true);
