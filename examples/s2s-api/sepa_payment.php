199a859dcb8710eca8c478e20614f0a8dd7ffcf6
pwolowiec-zz <piti.highlander@gmail.com>
2023-10-03 11:50:23 +0200

    NNEO-11817 - SEPA example (#29)

---
+++ examples/s2s-api/sepa_payment.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This example shows you how to process a Direct Debit (SEPA) transaction.
+ * You can use the payment detail token returned in this example to run the example in payment_with_saved_token.php
+ */
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+try {
+    $sepaPayment = new \VendoSdk\S2S\Request\Payment();
+    $sepaPayment->setApiSecret(getenv('VENDO_SECRET_API', true) ?: 'Your_vendo_secret_api');
+    $sepaPayment->setMerchantId(getenv('VENDO_MERCHANT_ID',  true) ?: 'Your_vendo_merchant_id');//Your Vendo Merchant ID
+    $sepaPayment->setSiteId(getenv('VENDO_SITE_ID' , true) ?: 'Your_vendo_site_id');//Your Vendo Site ID
+    $sepaPayment->setAmount(120.00);
+    $sepaPayment->setCurrency(\VendoSdk\Vendo::CURRENCY_EUR);
+    $sepaPayment->setIsTest(true);
+
+    //You must set the flag below to TRUE if you're processing a recurring billing transaction
+    $sepaPayment->setIsMerchantInitiatedTransaction(false);
+
+    //You may add non_recurring flag to mark no merchant initiated transactions (rebills) will follow, required by some banks
+    $sepaPayment->setIsNonRecurring(true);
+
+    //Set this flag to true when you do not want to capture the transaction amount immediately, but only validate the
+    // payment details and block (reserve) the amount. The capture of a preauth-only transaction can be performed with
+    // the CapturePayment class.
+    $sepaPayment->setPreAuthOnly(false);
+
+    $externalRef = new \VendoSdk\S2S\Request\Details\ExternalReferences();
+    $externalRef->setTransactionReference('your_tx_reference_123');
+    $sepaPayment->setExternalReferences($externalRef);
+
+    /**
+     * Add items to your request, you can add one or more
+     */
+    $cartItem = new \VendoSdk\S2S\Request\Details\Item();
+    $cartItem->setId(123);//set your product id
+    $cartItem->setDescription('Registration fee');//your product description
+    $cartItem->setPrice(4.00);
+    $cartItem->setQuantity(1);
+    $sepaPayment->addItem($cartItem);
+
+    $cartItem2 = new \VendoSdk\S2S\Request\Details\Item();
+    $cartItem2->setId(123);//set your product id
+    $cartItem2->setDescription('Unlimited video download');//your product description
+    $cartItem2->setPrice(6.50);
+    $cartItem2->setQuantity(1);
+    $sepaPayment->addItem($cartItem2);
+
+    /**
+     * Provide the Direct Debit (SEPA) details that you collected from the user
+     */
+    $sepaDetails = new \VendoSdk\S2S\Request\Details\PaymentMethod\Sepa();
+    $sepaDetails->setIban("DE89370400440532013000");
+    $sepaPayment->setPaymentDetails($sepaDetails);
+
+    /**
+     * Customer details
+     */
+    $customer = new \VendoSdk\S2S\Request\Details\Customer();
+    $customer->setFirstName('John');
+    $customer->setLastName('Doe');
+    $customer->setEmail('qa+bpsepa+test@vendoservices.com');
+    $customer->setLanguageCode('en');
+    $customer->setCountryCode('DE');
+    $sepaPayment->setCustomerDetails($customer);
+
+    /**
+     * Shipping details. This is required.
+     */
+    $shippingAddress = new \VendoSdk\S2S\Request\Details\ShippingAddress();
+    $shippingAddress->setFirstName($customer->getFirstName());
+    $shippingAddress->setLastName($customer->getLastName());
+    $shippingAddress->setAddress($customer->getAddress());
+    $shippingAddress->setCountryCode($customer->getCountryCode());
+    $shippingAddress->setCity($customer->getCity());
+    $shippingAddress->setState($customer->getState());
+    $shippingAddress->setPostalCode($customer->getPostalCode());
+    $shippingAddress->setPhone($customer->getPhone());
+    //If you're selling digital content then you are allowed to use dummy details like the ones below
+    $shippingAddress->setAddress('123 Example Strasse');
+    $shippingAddress->setCity('Berlin');
+    $shippingAddress->setState('FL');
+    $shippingAddress->setPostalCode('33000');
+    $shippingAddress->setPhone('1000000000');
+    $sepaPayment->setShippingAddress($shippingAddress);
+
+    /**
+     * User request details
+     */
+    $request = new \VendoSdk\S2S\Request\Details\ClientRequest();
+    $request->setIpAddress($_SERVER['REMOTE_ADDR'] ?: '127.0.0.1');//you must pass a valid IPv4 address
+    $request->setBrowserUserAgent($_SERVER['HTTP_USER_AGENT'] ?: null);
+    $sepaPayment->setRequestDetails($request);
+
+    $response = $sepaPayment->postRequest();
+
+    echo "\n\nRESULT BELOW\n";
+    if ($response->getStatus() == \VendoSdk\Vendo::S2S_STATUS_OK) {
+        echo "The transactions was successfully processed. Vendo's Transaction ID is: " . $response->getTransactionDetails()->getId();
+        echo "\nThe Direct Debit (SEPA) payment Auth Code is: " . $response->getCreditCardPaymentResult()->getAuthCode();
+        echo "\nThe Payment Details Token is: ". $response->getPaymentToken();
+        echo "\nYou must save the payment details token if you need or want to process future recurring billing or one-clicks\n";
+        echo "\nThis is your transaction reference (the one you set it in the request): " . $response->getExternalReferences()->getTransactionReference();
+    } elseif ($response->getStatus() == \VendoSdk\Vendo::S2S_STATUS_NOT_OK) {
+        echo "The transaction failed.";
+        echo "\nError message: " . $response->getErrorMessage();
+        echo "\nError code: " . $response->getErrorCode();
+    } elseif ($response->getStatus() == \VendoSdk\Vendo::S2S_STATUS_VERIFICATION_REQUIRED) {
+        echo "The transaction must be verified";
+        echo "\nYou MUST :";
+        echo "\n   1. Save the payment token: " . $response->getPaymentToken();
+        echo "\n   2. Redirect the user to the verification URL: " . $response->getResultDetails()->getVerificationUrl();
+        echo "\nthe user will verify his payment details, then he will be redirected to the Success URL that's configured in your account at Vendo's back office.";
+        echo "\nwhen the user comes back you need to post the request to vendo again, you can use the TokenPayment class.";
+    }
+    echo "\n\n\n";
+
+
+} catch (\VendoSdk\Exception $exception) {
+    die ('An error occurred when processing your API request. Error message: ' . $exception->getMessage());
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    die ('An error occurred when processing the HTTP request. Error message: ' . $e->getMessage());
+}
+
