4d93adf2a13c028a1b5c7054824e5fb211c3fca7
zbigniew.wiech <zbigniew.wiech@vendoservices.com>
2026-02-10 15:43:55 +0100

    sepa.iban field is optional

--- tests/S2S/SepaDetailsTest.php
+++ tests/S2S/SepaDetailsTest.php
@@ -2,25 +2,56 @@
 
 namespace VendoSdkUnit\S2S;
 
-use GuzzleHttp\Client;
-use Psr\Http\Message\ResponseInterface;
-use VendoSdk\S2S\Request\Details\PaymentMethod\Pix;
-use VendoSdk\S2S\Request\Details\ClientRequest;
 use VendoSdk\S2S\Request\Details\PaymentMethod\Sepa;
-use VendoSdk\Vendo;
 
 class SepaDetailsTest extends \PHPUnit\Framework\TestCase
 {
-    public function testSepaDetails()
+    public function testSepaDetailsWithIbanAndBicSwift()
     {
-        $pix = new Sepa();
-        $pix->setIban('DE1234000012340000');
-        $pix->setBicSwift('MYSWIFT000');
+        $sepa = new Sepa();
+        $sepa->setIban('DE1234000012340000');
+        $sepa->setBicSwift('MYSWIFT000');
 
         self::assertSame([
             'payment_method' => 'sepa',
             'iban' => 'DE1234000012340000',
             'bic_swift' => 'MYSWIFT000',
-        ], $pix->jsonSerialize());
+        ], $sepa->jsonSerialize());
+    }
+
+    public function testSepaDetailsWithNullIbanOmitsIbanFromPayload()
+    {
+        $sepa = new Sepa();
+        $sepa->setBicSwift('MYSWIFT000');
+
+        $result = $sepa->jsonSerialize();
+
+        self::assertArrayNotHasKey('iban', $result);
+        self::assertSame('sepa', $result['payment_method']);
+        self::assertSame('MYSWIFT000', $result['bic_swift']);
+    }
+
+    public function testSepaDetailsWithExplicitNullIbanOmitsIbanFromPayload()
+    {
+        $sepa = new Sepa();
+        $sepa->setIban(null);
+        $sepa->setBicSwift('MYSWIFT000');
+
+        $result = $sepa->jsonSerialize();
+
+        self::assertArrayNotHasKey('iban', $result);
+        self::assertSame('sepa', $result['payment_method']);
+    }
+
+    public function testSepaDetailsWithNoIbanAndNoBicOmitsBothFromPayload()
+    {
+        $sepa = new Sepa();
+
+        $result = $sepa->jsonSerialize();
+
+        self::assertArrayNotHasKey('iban', $result);
+        self::assertArrayNotHasKey('bic_swift', $result);
+        self::assertSame('sepa', $result['payment_method']);
+        self::assertSame(['payment_method' => 'sepa'], $result);
     }
 }
